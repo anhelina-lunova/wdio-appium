ba8fbac0220c1c97a6c2db3849dcb34e3402755e
Angie Lina <120516170+anhelina-lunova@users.noreply.github.com>
2026-05-06 19:01:08 +0300

    move files

---
+++ test/ios/specs/actions.e2e.js
@@ -0,0 +1,83 @@
+import { driver, expect } from "@wdio/globals";
+describe("Connection", () => {
+    it("First test", async () => {
+        await driver.pause(2000);
+    });
+});
+
+describe.skip("Actions", () => {
+    it("Get text", async () => {
+        console.log("Element text: " + (await $("~Accessibility").getText()));
+
+        await expect($("~Accessibility")).toHaveText("Accessibility");
+        await expect($("~Accessibility")).toHaveAttr("text", "Accessibility");
+        await driver.pause(2000);
+    });
+
+    it("Set text", async () => {
+        await $("~App").click();
+        await $("~Search").click();
+        await $("~Invoke Search").click();
+        await $("id=io.appium.android.apis:id/txt_query_prefill").setValue(
+            "Test text",
+        );
+
+        await expect(
+            $("id=io.appium.android.apis:id/txt_query_prefill"),
+        ).toHaveText("Test text");
+        await expect(
+            $("id=io.appium.android.apis:id/txt_query_prefill"),
+        ).toHaveAttr("text", "Test text");
+        await driver.pause(2000);
+    });
+
+    it("Dropdowns", async () => {
+        await $("~App").click();
+        await $("~Menu").click();
+        await $("~Inflate from XML").click();
+        await $("id=android:id/text1").click();
+        await $(
+            "//android.widget.CheckedTextView[@resource-id='android:id/text1' and @text='Order'] ",
+        ).click();
+
+        await expect($("android.widget.ListView")).not.toBeExisting();
+        await expect($("id=android:id/text1")).toHaveText("Order");
+        await driver.pause(2000);
+    });
+
+    it("Checkboxes and Radio Buttons", async () => {
+        await $("~Views").click();
+        await $("~Controls").click();
+        await $("~2. Dark Theme").click();
+        await expect($("id=io.appium.android.apis:id/check1")).toHaveAttr(
+            "checked",
+            "false",
+        );
+        await $("id=io.appium.android.apis:id/check1").click();
+        await expect($("id=io.appium.android.apis:id/check1")).toHaveAttr(
+            "checked",
+            "true",
+        );
+        await driver.pause(2000);
+    });
+
+    it("Multiple Elements", async () => {
+        const elements = await $$("android.widget.TextView");
+
+        for (const el of elements) {
+            console.log(await el.getText());
+        }
+
+        await expect(elements[1]).toHaveText("Access'ibility");
+        await driver.pause(2000);
+    });
+
+    it("Waits", async () => {
+        await $("~Views").click();
+        await $("~Chronometer").click();
+        await $("~Start").click();
+        await $("~5 seconds").waitForDisplayed();
+        await $("~Stop").click();
+        await driver.pause(2000);
+    });
+});

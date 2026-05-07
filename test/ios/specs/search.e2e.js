b119f954881ff68c89ab3445149c08f192f57ba6
Angie Lina <120516170+anhelina-lunova@users.noreply.github.com>
2026-05-07 10:10:51 +0300

    change selectors order by speed priority

--- test/ios/specs/search.e2e.js
+++ test/ios/specs/search.e2e.js
@@ -6,18 +6,10 @@ describe("Search elements", () => {
         await driver.pause(2000);
     });
 
-    it("by Class Name (type)", async () => {
-        const elements = await $$("XCUIElementTypeStaticText");
-        await expect(elements).toBeElementsArrayOfSize(37);
-    });
-
-    it("by Xpath", async () => {
-        await $('//XCUIElementTypeStaticText[@name="Image View"]').click();
-        await driver.pause(2000);
-    });
-
-    it("by ID (name)", async () => {
-        await $("id=Buttons").click();
+    it("by Class Chain", async () => {
+        await $(
+            '-ios class chain:**/XCUIElementTypeStaticText[`name == "Text Fields"`]',
+        ).click();
         await driver.pause(2000);
     });
 
@@ -26,10 +18,18 @@ describe("Search elements", () => {
         await driver.pause(2000);
     });
 
-    it("by Class Chain", async () => {
-        await $(
-            '-ios class chain:**/XCUIElementTypeStaticText[`name == "Text Fields"`]',
-        ).click();
+    it("by ID (name)", async () => {
+        await $("id=Buttons").click();
+        await driver.pause(2000);
+    });
+
+    it("by Class Name (type)", async () => {
+        const elements = await $$("XCUIElementTypeStaticText");
+        await expect(elements).toBeElementsArrayOfSize(37);
+    });
+
+    it("by Xpath", async () => {
+        await $('//XCUIElementTypeStaticText[@name="Image View"]').click();
         await driver.pause(2000);
     });
 });
